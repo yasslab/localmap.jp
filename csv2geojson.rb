c77c581471812abbf0b1948a77a369ff458c9afe
Junya Ishihara <junya@champierre.com>
2022-12-08 01:19:02 +0900

    first commit

---
+++ csv2geojson.rb
@@ -0,0 +1,27 @@
+require 'json'
+require 'csv'
+
+posts = CSV.parse(File.read("posts.csv"), headers: true)
+
+features = []
+posts.each do |post|
+  features << {
+    "type" => "Feature",
+    "geometry" => {
+      "type" => "Point",
+      "coordinates" => [post["lng"], post["lat"]]
+    },
+    "properties" => {
+      "description" => "<a target='_blank' href='#{post['url']}'>#{post['title']}</a>"
+    }
+  }
+end
+
+geojson = {
+  "type": "FeatureCollection",
+  "features": features
+}
+
+File.open("posts.geojson", "w") do |file|
+  JSON.dump(geojson, file)
+end
