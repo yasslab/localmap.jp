cb069e174f5f7d660b041494c208b6796aebe0a8
Yohei Yasukawa <yohei@yasslab.jp>
2022-12-19 21:07:24 +0900

    Add caching system for less burden on servers

--- upsert_geojson.rb
+++ upsert_geojson.rb
@@ -4,41 +4,28 @@
 require 'json'
 require 'mechanize'
 
-BASE_URL = 'https://takadanobaba.keizai.biz/mapnews/'
-#BASE_MAP = 'https://maps.google.com/maps?q='
-agent = Mechanize.new
+MARKERS_YAML = 'markers.yaml'
+BASE_URL     = 'https://takadanobaba.keizai.biz/mapnews/'
 
-features = []
-skipped_ids = [958]
-#article_ids = [1000,997,996,993,987,984,979,977,970,964] # for debugging
-#article_ids.each do |id|
-(900..1000).each do |id|
-  next if skipped_ids.include? id
-  sleep(10) if id % 11 == 0 # Add interbal to fetch for servers
+mechanize   = Mechanize.new
+features    = []
+marker_data = YAML.unsafe_load_file(MARKERS_YAML, symbolize_names: true)
 
-  html  = agent.get(BASE_URL + id.to_s)
-  date  = html.search('time').text
-  link  = html.search('li.send a').attribute('href').value
-  title = html.search('h1').last.text
-  #lng,lat = html.search('p#mapLink a').attribute('href').value.delete(BASE_MAP).split(',')
-  lat,lng = html.search('p#mapLink a').attribute('href').value[31..].split(',')
-  p "[#{id}] #{title} "
-
-  #binding.irb
-  #exit
+marker_data.each do |marker|
+  next if marker[:title].eql? '404_Not_Found'
 
   features << {
     type: "Feature",
     geometry: {
       type: "Point",
-      coordinates: [lng, lat]
+      coordinates: [marker[:lng], marker[:lat]]
     },
     properties: {
       'marker-size'   => 'small',
-      #'marker-symbol' => 'minkei',
+      'marker-symbol' => 'minkei',
       'marker-color'  => 'rgba(45, 105, 176, 0.7)', # BabaKeizai Blue
       #'marker-color' => 'rgba(237, 208, 70, 0.8)', # BabaKeizai Yellow
-      description: "<a target='_blank' rel='noopener' href='#{link}'>#{title}</a> <small>(#{date})</small>"
+      description: "<a target='_blank' rel='noopener' href='#{marker[:link]}'>#{marker[:title]}</a> <small>(#{marker[:date]})</small>"
     }
   }
 end
@@ -48,7 +35,7 @@ geojson = {
   "features": features
 }
 
-File.open("posts.geojson", "w") do |file|
+File.open("markers.geojson", "w") do |file|
   JSON.dump(geojson, file)
 end
 
---
+++ upsert_markers.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'yaml'
+require 'mechanize'
+
+MARKERS_YAML = 'markers.yaml'
+BASE_URL     = 'https://takadanobaba.keizai.biz/mapnews/'
+#BASE_MAP    = 'https://maps.google.com/maps?q='
+
+mechanize                  = Mechanize.new
+mechanize.user_agent_alias = 'Windows Chrome'
+existing_marker_ids = YAML.unsafe_load_file(MARKERS_YAML) ?
+                      YAML.unsafe_load_file(MARKERS_YAML).map{|h| h['id']} :
+                      [0]
+upserted_marker_data = File.read('markers.yaml')
+
+(1..10).each do |id|
+  next if existing_marker_ids.include? id
+
+  begin
+    html  = mechanize.get(BASE_URL + id.to_s)
+  rescue Mechanize::ResponseCodeError => error
+    puts 'ERROR: ' + error.response_code
+  end
+
+  if html.search('time').text.empty? == false
+    date  = html.search('time').text
+    link  = html.search('li.send a').attribute('href').value
+    title = html.search('h1').last.text
+    lat,lng = html.search('p#mapLink a').attribute('href').value[31..].split(',')
+    puts format("[%04d] #{title}", id)
+
+    upserted_marker_data << <<~NEW_MARKER
+    - id:    #{id}
+      link:  #{link}
+      date:  #{date}
+      title: '#{title}'
+      lat:   #{lat}
+      lng:   #{lng}
+    NEW_MARKER
+  else
+    puts "[#{id.to_s.rjust(4, '0')}] 404 Not Found"
+    upserted_marker_data << <<~NEW_MARKER
+    - id:    #{id}
+      link:  https://takadanobaba.keizai.biz/
+      date:  2000-01-23
+      title: 404_Not_Found
+      lat:   35.7120933
+      lng:   139.7047394
+    NEW_MARKER
+  end
+
+  #puts upserted_marker_data
+end
+
+IO.write(MARKERS_YAML, upserted_marker_data) unless upserted_marker_data.empty?
